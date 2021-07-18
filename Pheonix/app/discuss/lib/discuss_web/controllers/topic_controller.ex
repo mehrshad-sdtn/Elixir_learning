59af98912b85b5718e443fe4bc9f1ae50ce64cba
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-18 12:51:05 +0430

    Update topic_controller.ex

--- Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,14 @@
 defmodule Discuss.TopicController do
   use DiscussWeb, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
+
+
 
   end
 
