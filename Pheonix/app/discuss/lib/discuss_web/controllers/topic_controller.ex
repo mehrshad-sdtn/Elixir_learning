bddde5c8e0fa797f8754b5158e72d124b99b17e5
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-23 23:50:25 +0430

    query all topics

--- Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,11 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
