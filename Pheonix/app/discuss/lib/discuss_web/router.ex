f1a79e9e4d1b1d204d67120ebf2f1365c5d895cf
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-24 23:44:18 +0430

    new route for edit

--- Pheonix/app/discuss/lib/discuss_web/router.ex
+++ Pheonix/app/discuss/lib/discuss_web/router.ex
@@ -19,6 +19,7 @@ defmodule DiscussWeb.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
 
   end
 
