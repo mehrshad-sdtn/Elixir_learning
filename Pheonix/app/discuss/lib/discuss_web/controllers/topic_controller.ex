ee3984698e12bd7cd89cc89ffa627c00d713cdca
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-25 01:05:31 +0430

    Update topic_controller.ex

--- Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -35,7 +35,21 @@ defmodule DiscussWeb.TopicController do
     render conn, "edit.html", changeset: changeset, topic: topic
   end
 
-  def update(conn, %{"id" => topic_id}) do
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset =
+    Topic
+    |> Repo.get(topic_id)
+    |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset
+    end
 
   end
 
