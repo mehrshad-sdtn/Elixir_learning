7306a5b127ab04e6250711d671dfd3677ed53258
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-26 22:36:18 +0430

    delete added

--- Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -51,4 +51,13 @@ defmodule DiscussWeb.TopicController do
 
   end
 
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
+
 end
