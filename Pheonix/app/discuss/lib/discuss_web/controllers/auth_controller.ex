90473e0048b13337d2e0b27ce2f898dc55c784a8
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-29 21:14:48 +0430

    Update auth_controller.ex

--- Pheonix/app/discuss/lib/discuss_web/controllers/auth_controller.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,6 +2,9 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
+  alias Discuss.User
+  alias Discuss.Repo
+
   def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
     conn
     |> put_flash(:error, "Failed to authenticate.")
@@ -9,9 +12,23 @@ defmodule DiscussWeb.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    IO.puts "++++++++++++++++++++++"
-    IO.inspect(conn.assigns)
-    IO.puts "+++++++++++++++++++++"
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "google"
+    }
+    changeset = User.changeset(%User{}, user_params)
+    insert_or_update_user(changeset)
+  end
+
+
+  defp insert_or_update_user(changeset) do
+   case  Repo.get_by(User, email: changeset.changes.email) do
+    nil -> Repo.insert(changeset)
+    user -> {:ok, user}
+   end
+
+
   end
 
 end
