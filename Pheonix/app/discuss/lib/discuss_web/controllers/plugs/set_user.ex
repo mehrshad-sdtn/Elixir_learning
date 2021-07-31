12bfe6be1d11ad914acd83238ae3580761237d98
mehrshad_sdt <saadatiniamehrshad@gmail.com>
2021-07-31 14:09:27 +0430

    Update set_user.ex

--- Pheonix/app/discuss/lib/discuss_web/controllers/plugs/set_user.ex
+++ Pheonix/app/discuss/lib/discuss_web/controllers/plugs/set_user.ex
@@ -9,11 +9,12 @@ defmodule DiscussWeb.Plugs.SetUser do
 
   end
   def init(conn, _params) do
-    user_id = get_session(conn, :user_id)
+    user_id = get_session(conn, :user_id) #from Pheonix.Controller
+
 
     cond do
       user = user_id && Repo.get(User, user_id) ->
-        assign(conn, :user, user)
+        assign(conn, :user, user) #from Plug.Conn
 
       true ->
         assign(conn, :user, nil)
